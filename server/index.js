7248740c7c07a9ba2995d64af371cfa2e2141e7d
muhzina98 <muhzinan98@gmail.com>
2025-11-20 17:20:58 +0530

    latest commit

--- server/index.js
+++ server/index.js
@@ -1,47 +1,40 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
-const port = process.env.PORT  || 3001;
+const port = process.env.PORT || 3001;
 const connectDatabase = require('./config/dbConnection')
 const router = require('./routes/midRoute')
-const cookieParser =require('cookie-parser')
-const cors= require('cors')
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-connectDatabase()
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://movie-review-rating-site-1.onrender.com",
-  "https://movie-review-rating-site.onrender.com"
-];
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS not allowed for this origin: " + origin));
-    }
-  },
+  origin: [
+    "http://localhost:5173",
+    "https://movie-review-rating-site-1.onrender.com",
+    "https://movie-review-rating-site.onrender.com"
+  ],
+  credentials: true
+}));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://movie-review-rating-site-1.onrender.com",
+    "https://movie-review-rating-site.onrender.com"
+  ],
   credentials: true
 }));
 
 
-
-
-app.use(cookieParser())
-app.use(express.json())
+app.use(cookieParser());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => res.send('Hello World!'));
 
+connectDatabase();
 
-
-//http://localhost:3001/api
-app.use('/api',router)
-
+app.use('/api', router);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-    });
+  console.log(`Server running on port ${port}`)
+});
