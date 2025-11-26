bb9c40ea8ac39dbf440cef566a50a70b19f399bb
muhzina98 <muhzinan98@gmail.com>
2025-11-26 20:32:26 +0530

    updated file

--- server/index.js
+++ server/index.js
@@ -17,14 +17,14 @@ connectDatabase();
 // API routes FIRST
 app.use("/api", router);
 
-//Serve React frontend build
-const frontendPath = path.join(__dirname, "..", "client", "dist");
-app.use(express.static(frontendPath));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/dist")));
 
-// Fallback route (for all other paths)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
-});
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+  });
+}
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
