6d956a99a65ac5daae930c6e738e94adb6a3d902
muhzina98 <muhzinan98@gmail.com>
2025-11-26 20:14:28 +0530

    updated file

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use("/api", router);
 // Serve React frontend
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
