58228f350abc231736eecd6b4082bdf9b26b12a2
muhzina98 <muhzinan98@gmail.com>
2025-11-27 00:20:23 +0530

    updated file

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: `${import.meta.env.VITE_BASE_URL}/api`,/
-    baseURL: "/api",
+   baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
+    //baseURL: "/api",
 
   withCredentials: true,
 });
